7c76b796b74130b08528be4385ec9b2bd66c779c
enzochave <enzo.moraes6@aluno.senai.br>
2025-12-05 15:29:36 -0300

    novembro

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -2,10 +2,7 @@ function enviarWhatsApp() {
     const numero = document.getElementById("numero").value;
     const mensagem = document.getElementById("mensagem").value;
 
-    if (!numero) {
-        alert("Digite um número!");
-        return;
-    }
+   
 
     const texto = encodeURIComponent(mensagem);
     const link = `https://wa.me/${numero}?text=${texto}`;
